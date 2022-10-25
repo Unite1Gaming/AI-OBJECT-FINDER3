0897838190ddeddf797dda78755e4043fa7a393f
Unite1Gaming <103253549+Unite1Gaming@users.noreply.github.com>
2022-10-25 19:58:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+objects = [];
+status = "";
+video = "";
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(480, 380);
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+    if(status !="")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML= "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label == object_name)
+            {
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = "Object Found";
+            }
+            else
+            {
+                document.getElementById("status").innerHTML = "Object Not-Found";
+            }
+        }
+    }
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
